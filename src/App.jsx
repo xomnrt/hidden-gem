1cf1bd47e32d72671bf2974bfa3ce50aad0779db
xomnrt <xomnrt1@ya.ru>
2025-04-07 18:13:48 +0300

    set tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ function App() {
 
   return (
     <>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
